9076d401708a3d8da868a0a05f519c7d5866771f
syscrusher <syscrusher@users.noreply.github.com>
2016-03-01 22:17:07 -0600

    Update package comment to reflect README updates.

--- fake.go
+++ fake.go
@@ -1,38 +1,3 @@
-/*
-Package fake is the fake data generatror for go (Golang), heavily inspired by forgery and ffaker Ruby gems
-
-Most data and methods are ported from forgery/ffaker Ruby gems.
-
-Currently english and russian languages are available.
-
-For the list of available methods please look at https://godoc.org/github.com/syscrusher/fake.
-
-Fake embeds samples data files unless you call UseExternalData(true) in order to be able to work without external files dependencies when compiled, so, if you add new data files or make changes to existing ones don't forget to regenerate data.go file using github.com/mjibson/esc tool and esc -o data.go -pkg fake data command (or you can just use go generate command if you are using Go 1.4 or later).
-
-Examples:
-	name := fake.FirstName()
-	fullname = := fake.FullName()
-	product := fake.Product()
-
-Changing language:
-	err := fake.SetLang("ru")
-	if err != nil {
-		panic(err)
-	}
-	password := fake.SimplePassword()
-
-Using english fallback:
-	err := fake.SetLang("ru")
-	if err != nil {
-		panic(err)
-	}
-	fake.EnFallback(true)
-	password := fake.Paragraph()
-
-Using external data:
-	fake.UseExternalData(true)
-	password := fake.Paragraph()
-*/
 package fake
 
 import (
